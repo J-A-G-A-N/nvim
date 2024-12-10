915db40c1c75c7b46816c5214dbcee068f4360c4
J-A-G-A-N <jaganp5002@gmail.com>
2024-12-10 20:53:51 +0530

    added gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,48 +1,47 @@
--- return {"ellisonleao/gruvbox.nvim", priority = 1000 , 
---   config = function()
---         vim.cmd.colorscheme "gruvbox"
---  end
--- }
--- return {
---     "dylanaraps/wal.vim",
---     configs = function()
---         local config = require("wal")
---         vim.cmd.colorscheme "wal"
---     end
--- }
-return{ "catppuccin/nvim", name = "catppuccin", priority = 1000 ,
-    config = function()
-    require("catppuccin").setup({
-        flavour = "macchiato", -- latte, frappe, macchiato, mocha ,auto
-        background = { -- :h background
-            light = "latte",
-        },
-        transparent_background = true, -- disables setting the background color.
-        show_end_of_buffer = false, -- shows the '~' characters after the end of buffers
-        term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
-        dim_inactive = {
-            enabled = false, -- dims the background color of inactive window
-            shade = "light",
-            percentage = 0.15, -- percentage of the shade to apply to the inactive window
-        },
-        color_overrides = {},
-        custom_highlights = {},
-        default_integrations = true,
-        integrations = {
-            cmp = true,
-            gitsigns = true,
-            nvimtree = true,
-            treesitter = true,
-            notify = false,
-            mini = {
-                enabled = true,
-                indentscope_color = "",
-            },
-            -- For more plugins integrations please scroll down (https://github.com/catppuccin/nvim#integrations)
-        },
-})
+return {
+	{
+		"ellisonleao/gruvbox.nvim",
+		name = "gruvbox",
+		priority = 1000 , 
 
--- setup must be called before loading
-vim.cmd.colorscheme "catppuccin"
-    end
+	},
+
+	{
+		"dylanaraps/wal.vim",
+	},
+	{
+		"catppuccin/nvim", name = "catppuccin", priority = 1000 ,
+		config = function()
+		require("catppuccin").setup({
+			flavour = "macchiato", -- latte, frappe, macchiato, mocha ,auto
+			background = { -- :h background
+				light = "latte",
+			},
+			transparent_background = true, -- disables setting the background color.
+			show_end_of_buffer = true, -- shows the '~' characters after the end of buffers
+			term_colors = false, -- sets terminal colors (e.g. `g:terminal_color_0`)
+			dim_inactive = {
+				enabled = false, -- dims the background color of inactive window
+				shade = "light",
+				percentage = 0.15, -- percentage of the shade to apply to the inactive window
+			},
+			color_overrides = {},
+			custom_highlights = {},
+			default_integrations = true,
+			integrations = {
+				cmp = true,
+				gitsigns = true,
+				nvimtree = true,
+				treesitter = true,
+				notify = true,
+				mini = {
+					enabled = true,
+					indentscope_color = "",
+				},
+			},
+	})
+	-- setup must be called before loading
+	vim.cmd.colorscheme "catppuccin"
+		end
+	}
 }
